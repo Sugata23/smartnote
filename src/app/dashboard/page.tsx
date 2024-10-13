956ac15d19c59324d45371ddea65a01a699d722d
Sugata <sarkarsugata57@gmail.com>
2024-10-14 00:49:52 +0530

    notes creation

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,8 @@
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { UserButton } from '@clerk/nextjs'
+import { ArrowLeft } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {}
@@ -7,7 +12,24 @@ function Dashboard({ }: Props) {
         <div className='grainy min-h-screen'>
             <div className='max-w-7xl mx-auto p-10'>
                 <div className="h-14"></div>
-                <div className='flex items-center justify-between md:flex-row flex-col'></div>
+                <div className='flex items-center justify-between md:flex-row flex-col'>
+                    <div className='flex items-center'>
+                        <Link href='/'>
+                            <Button className='bg-green-600'><ArrowLeft className='mr-1 w-4 h-4' />Back</Button>
+                        </Link>
+                        <div className="w-4"></div>
+                        <h1 className='text-3xl font-bold text-gray-900'>My Notes</h1>
+                        <div className="w-4"></div>
+                        <UserButton />
+                    </div>
+                </div>
+                <div className="h-8"></div>
+                <Separator />
+                <div className="h-8"></div>
+                {/* list all the notes */}
+                <div className='text-center'>
+                    <h1 className='text-xl text-gray-500'>You have no notes yet.</h1>
+                </div>
             </div>
         </div>
     )
